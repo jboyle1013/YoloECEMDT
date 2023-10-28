371a5c8b8011830abf753e9417722bd8bb5044ed
jboyle1013@vt.edu <58829335+jboyle1013@users.noreply.github.com>
2023-10-27 20:13:16 -0400

    FixedWebCamTest

--- WebcamTest.py
+++ WebcamTest.py
@@ -1,13 +1,25 @@
 from ultralytics import YOLO
 import cv2
 import math
+import time
+from imutils.video import FPS
 # start webcam
+print("[INFO] starting video stream...")
 cap = cv2.VideoCapture(0)
+
+# warm up the camera for a couple of seconds
+time.sleep(2.0)
+
+# FPS: used to compute the (approximate) frames per second
+# Start the FPS timer
+fps = FPS().start()
+
+# loop over the frames from the video stream
 cap.set(3, 640)
 cap.set(4, 480)
 
 # model
-model = YOLO("runs/detect/train43/weights/best.pt")
+model = YOLO("train43/weights/best.pt")
 
 # object classes
 classNames = ['BigBox', 'Nozzle', 'Rocket', 'SmallBox']
@@ -22,29 +34,32 @@ while True:
         boxes = r.boxes
 
         for box in boxes:
-            # bounding box
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-            # put box in cam
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-
-            # confidence
             confidence = math.ceil((box.conf[0]*100))/100
-            print("Confidence --->",confidence)
+            if confidence > 0.6:
 
-            # class name
-            cls = int(box.cls[0])
-            print("Class name -->", classNames[cls])
+                # bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-            # object details
-            org = [x1, y1]
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            fontScale = 1
-            color = (255, 0, 0)
-            thickness = 2
+                # put box in cam
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+                # confidence
+                print("Confidence --->",confidence)
+
+                # class name
+                cls = int(box.cls[0])
+                print("Class name -->", classNames[cls])
+
+                # object details
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+
+                cv2.putText(img, f"{classNames[cls]}: {confidence}", org, font, fontScale, color, thickness)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
